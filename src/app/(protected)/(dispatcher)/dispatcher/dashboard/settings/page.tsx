ed67f685610f6bb796c1bbd49caa8156d72c5b63
alshohid <alshohid1997@gmail.com>
2026-04-19 17:40:14 +0600

    add deropzone

--- src/app/(protected)/(dispatcher)/dispatcher/dashboard/settings/page.tsx
+++ src/app/(protected)/(dispatcher)/dispatcher/dashboard/settings/page.tsx
@@ -1,10 +1,11 @@
 import SettingsContainer from "@/src/components/admin/settings/SettingsContainer";
+import { Suspense } from "react";
 
 
 export default function DispatcherSettingsPage() {
   return (
-    <div>
+    <Suspense fallback={<div>Loading...</div>}>
       <SettingsContainer />
-    </div>
+    </Suspense>
   );
 }
